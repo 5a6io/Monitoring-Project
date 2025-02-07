0014d53366585d4aff49247b2922ff34a7c120de
5a6io <yhm54321@naver.com>
2025-02-07 18:08:11 +0900

    modify .gitignore

--- demo/demo/src/test/java/com/example/demo/user/UserServiceTest.java
+++ demo/demo/src/test/java/com/example/demo/user/UserServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,16 +29,23 @@ class UserServiceTest {
         User findUser = repository.findByEmail(user.getEmail());
 
         //then
-        assertThat(user.getUsername()).isEqualTo("Lucy");
+        assertThat(findUser.getUsername()).isEqualTo("Lucy");
     }
 
     @Test
     void login() {
         //given
+        User user = new User();
+        user.setEmail("test@naver.com");
+        user.setUsername("Lucy");
+        user.setPassword("test1234");
 
         //when
+        repository.save(user);
+        User findUser = repository.findByEmail(user.getEmail());
 
         //then
-
+        assertThat(findUser.getEmail()).isEqualTo(user.getEmail());
+        assertThat(findUser.getPassword()).isEqualTo(user.getPassword());
     }
 }
