1fba71695a7c9ce4100975a88e514390d8052090
5a6io <yhm54321@naver.com>
2025-02-25 21:41:02 +0900

    update demo

--- demo/src/main/java/com/example/demo/user/SaveRequestDto.java
+++ demo/src/main/java/com/example/demo/user/SaveRequestDto.java
@@ -1,8 +1,12 @@
 package com.example.demo.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SaveRequestDto {
 
     private String username;
