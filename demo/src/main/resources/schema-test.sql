1ab3e4bff66cc27cb7ed095f61dc277f25a982ee
5a6io <yhm54321@naver.com>
2025-03-13 03:50:03 +0900

    update demo

--- demo/src/main/resources/schema-test.sql
+++ demo/src/main/resources/schema-test.sql
@@ -11,8 +11,6 @@ create table if not exists test.users (
     constraint idx_email unique (email)
 );
 
-insert into test.users (username, password, email) values ('test', 'test@1234', 'test@example.com');
-
 create table if not exists test.questions (
     qid bigserial primary key,
     email varchar(20) not null,
